b6b0b6ec633d9fc54a656f3b687f823ba1edc62c
Rafael de Figueiredo Alves <rafael.figueiredo.alves@gmail.com>
2023-09-02 22:20:40 -0300

    Update September 2, 2023
    => Fixed a small bug in eFirebaseRaltime.pas

--- source/eFirebase.pas
+++ source/eFirebase.pas
@@ -1,10 +1,10 @@
 unit eFirebase;
 
 //--------------------------------------------------------------------------------
-//  eFirebase - versão 1.0.5
+//  eFirebase - versão 1.1.1
 //  --------------------------
 //  Biblioteca para consumo de recursos do Firebase
-//  Desenvolvido por Rafael de Figueiredo Alves (DevPegasus)
+//  Desenvolvida por Rafael de Figueiredo Alves (DevPegasus)
 //  -------------------------------------------------------
 //  Dedicatória: "Dedico esta biblioteca aos grandes amores da minha vida, minha esposa Jailza e meus filhos Rafaela e Davi."
 //--------------------------------------------------------------------------------
@@ -28,7 +28,7 @@ Type
   End;
 
  const
-  eFirebase_VERSION = '1.1.0';
+  eFirebase_VERSION = '1.1.1';
 
 implementation
 
@@ -39,37 +39,82 @@ uses
 
 { TeFirebase }
 
-function TeFirebase.Auth(const API_Key: string): ieFirebaseAuth;
-begin
-  Result := TeFirebaseAuth.New(API_Key);
-end;
-
+/// <summary>
+///   Método Construtor. Use o método New preferencialmente
+/// </summary>
 constructor TeFirebase.Create;
 begin
 
 end;
 
+/// <summary>
+///   Método destruidor. Não utilize-o
+/// </summary>
 destructor TeFirebase.Destroy;
 begin
 
   inherited;
 end;
 
+/// <summary>
+///   Método inicializador da lib
+/// </summary>
+/// <returns>
+///   Retorna a interface de métodos da lib
+/// </returns>
 class function TeFirebase.New: ieFirebase;
 begin
   Result := Self.Create;
 end;
 
+/// <summary>
+///  Método para acessar as funções do Firebase Auth
+/// </summary>
+/// <param name="API_Key">
+/// Chave da API obtida nas opções de seu projeto no Firebase
+/// </param>
+/// <returns>
+///   Retorna interface das funções associadas a autenticação Firebase
+/// </returns>
+function TeFirebase.Auth(const API_Key: string): ieFirebaseAuth;
+begin
+  Result := TeFirebaseAuth.New(API_Key);
+end;
+
+/// <summary>
+///  Método para acessar as funções do Firebase RealTime Database
+/// </summary>
+/// <param name="ProjectCode">
+/// o Código ID do seu projeto no Firebase
+/// </param>
+/// <returns>
+///  Retorna interface das funções associadas ao RealTime DataBase
+/// </returns>
 function TeFirebase.RealTimeDB(const ProjectCode: string): ieFirebaseRealtime;
 begin
   Result := TeFirebaseRealTimeDB.New(ProjectCode);
 end;
 
+/// <summary>
+///  Método para acessar as funções do Firebase Storage
+/// </summary>
+/// <param name="ProjectCode">
+/// o Código ID do seu projeto no Firebase
+/// </param>
+/// <returns>
+///  Retorna interface das funções associadas ao Firebase Storage
+/// </returns>
 function TeFirebase.Storage(const ProjectCode: string): ieFirebaseStorage;
 begin
   Result := TeFirebaseStorage.New(ProjectCode);
 end;
 
+/// <summary>
+///  Método para obter a versão da Biblioteca
+/// </summary>
+/// <returns>
+///  Retorna uma string com informações da versão da biblioteca
+/// </returns>
 class function TeFirebase.Version: string;
 begin
   Result := eFirebase_VERSION;
